e7a30fd39725fa7ff8ef5f7d5f97dfa68f30e0c6
treezyxd <thalesmaltaxd@gmail.com>
2023-09-21 01:52:11 -0300

    add rota post/sign-up

--- src/app.js
+++ src/app.js
@@ -6,7 +6,25 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+let users = [];
 
+app.post('/sign-up', (req, res) => {
+  const { username, avatar } = req.body;
+
+  if (typeof username != "string" || typeof avatar != "string" || !username || !avatar) {
+    res.status(400).send('Todos os campos sao obrigatorios!');
+    return;
+  }
+
+  const newUser = {
+    username,
+    avatar
+  }
+
+  users.push(newUser);
+  
+  res.status(201).send('OK');
+})
 
 const PORT = 5000;
 app.listen(PORT, ()=>console.log(`server running at port ${PORT}`));

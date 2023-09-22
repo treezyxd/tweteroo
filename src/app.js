e3111906c876d5efcf8459d06099d1763648d168
treezyxd <thalesmaltaxd@gmail.com>
2023-09-22 00:51:25 -0300

    add rota post/tweet

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ app.use(express.json());
 app.use(cors());
 
 let users = [];
+let tweets = [];
 
 app.post('/sign-up', (req, res) => {
   const { username, avatar } = req.body;
@@ -25,6 +26,32 @@ app.post('/sign-up', (req, res) => {
   
   res.status(201).send('OK');
 })
+
+app.post('/tweets', (req, res) => {
+  const { username, tweet } = req.body;
+
+  if (typeof username != 'string' || typeof tweet != 'string' ||  !username || !tweet) {
+    res.status(400).send('Todos os campos sao obrigatorios');
+    return;
+  }
+
+  const userAuthorized = users.find(user => user.username === username);
+
+  if (!userAuthorized) {
+    return res.status(401).send('UNAUTHORIZED');
+  }
+
+  const newTweet = {
+    username,
+    tweet
+  }
+
+  tweets.push(newTweet);
+
+  res.status(201).send('OK');
+})
+
+
 
 const PORT = 5000;
 app.listen(PORT, ()=>console.log(`server running at port ${PORT}`));
